e10ec7db3c70bf9d1bffe0cd5ff696500b195320
zoverlvx <zoverhulser@gmail.com>
2020-05-27 17:33:41 -0400

    Begins set up for posting event data.

--- server/routes/events/index.js
+++ server/routes/events/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const getEvents = require("./getEvents.js");
+const postEvent = require("./postEvent.js");
 
 router.get("/", getEvents);
+router.post("/", postEvent);
 
 module.exports = router;
---
+++ server/routes/events/postEvent.js
@@ -0,0 +1,5 @@
+const Events = require("../../models")("events");
+
+module.exports = function(req, res) {
+	console.log(req.body)
+}
