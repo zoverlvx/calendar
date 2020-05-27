655f45c396cb2d0910a4dff96649d38a2bcb2467
zoverlvx <zoverhulser@gmail.com>
2020-05-27 18:47:42 -0400

    CORS issue like no other.

--- server/config/configureServer.js
+++ server/config/configureServer.js
@@ -2,8 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const whitelist = ["http://localhost:3000"];
+
 module.exports = function(server) {
-	server.use(cors());
+	server.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header(
+			"Access-Control-Allow-Headers", 
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		next();
+	});
+	server.use(cors({
+		origin: function(origin, cb) {
+			// allow requests with no origin.
+			if (!origin) return cb(null, true);
+			// if it's not on the whitelist, reject it.
+			if (whitelist.indexOf(origin) === -1) {
+				const message = "The CORS policy for this origin doesn't" 
+					+ " allow access from this particular origin";
+				return cb(new Error(message), false);
+			}
+			return cb(null, true);
+		}
+	}));
 	server.use(helmet());
 	server.use(express.json());
 	server.use(express.urlencoded({extended: false}));
