1da13bf70551a88117117057f3aa41609eae79df
zoverlvx <zoverhulser@gmail.com>
2020-05-27 15:04:47 -0400

    Adds, updates, deletes appointments on frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import { 
+	ViewState,
+	EditingState,
+	IntegratedEditing
+} from "@devexpress/dx-react-scheduler";
 import {
 	Scheduler,
 	DayView,
-	Appointments
+	WeekView,
+	MonthView,
+	Appointments,
+	Toolbar,
+	DateNavigator,
+	TodayButton,
+	ViewSwitcher,
+	AppointmentTooltip,
+	AppointmentForm,
+	ConfirmationDialog
 } from "@devexpress/dx-react-scheduler-material-ui";
-
-const currentDate = "2018-11-01";
-const schedulerData = [
-	{
-		startDate: "2018-11-01T09:45",
-		endDate: "2018-11-01T11:00",
-		title: "Meeting"
-	},
-	{
-		startDate: "2018-11-01T12:00",
-		endDate: "2018-11-01T13:30",
-		title: "Go to the gym"
-	}
-];
+import data from "./data";
 
 export default function(props) {
+	const [state, setState] = useState({
+		data,
+		currentViewName: "work-week"
+	});
+	function currentViewNameChange(currentViewName) {
+		setState({...state, currentViewName});
+	}
+	function commitChanges({ added, changed, deleted }) {
+		setState(function(state) {
+			const {data} = state;
+			let newData;
+			if (added) {
+				// axios.post("/postingendpoint", added)
+				//   .then(response => console.log(response))
+				//   .catch(error => alert(error))
+				console.log("here is added: ",added);
+				newData = [...data, {...added}];
+				// id is handled on the backend and will return the new object
+			} else if (changed) {
+				newData = data.map(function(appointment) {
+					return changed[appointment.id] 
+						? {...appointment, ...changed[appointment.id]}
+						: appointment;
+				});
+				console.log("here is the new data after change: ", newData);
+				console.log("here is changed: ", changed);
+
+			} else if (deleted !== undefined) {
+				newData = data.filter(function(appointment) {
+					return appointment.id !== deleted;
+				});
+				console.log("here is deleted: ", deleted);
+			}
+			return {...state, data: newData};
+		})
+	}
+	const today = new Date();
+
 	return (
 		<Paper>
 			<Scheduler
-				data={schedulerData}
+				data={state.data}
+				height={660}
 			>
 				<ViewState 
-					currentDate={currentDate}
+					defaultCurrentDate={today}
+					currentViewName={
+						state.currentViewName
+					}
+					onCurrentViewNameChange={
+						currentViewNameChange
+					}
 				/>
-				<DayView 
-					startDayHour={9}
-					endDayHour={14}
+				<EditingState 
+					onCommitChanges={commitChanges}
 				/>
+				<IntegratedEditing />
+
+				<WeekView 
+					name="work-week"
+					displayName="Work Week"
+					excludedDays={[0, 6]}
+					startDayHour={8}
+					endDayHour={17}
+				/>
+				<MonthView />
+				<DayView />
+				<ConfirmationDialog />
+				<Toolbar />
+				<DateNavigator />
+				<TodayButton />
+				<ViewSwitcher />
 				<Appointments />
+				<AppointmentTooltip 
+					showCloseButton
+					showOpenButton
+				/>
+				<AppointmentForm 
+					//appointmentData
+				/>
 			</Scheduler>
 		</Paper>
 	);
---
+++ client/src/data/index.js
@@ -0,0 +1,19 @@
+export default [
+  {
+	id: "unique1",
+    title: 'Website Re-Design Plan',
+    startDate: new Date(2020, 4, 22, 9, 30),
+    endDate: new Date(2020, 4, 22, 11, 30)
+  }, {
+	id: "unique2",
+    title: 'Book Flights to San Fran for Sales Trip',
+    startDate: new Date(2020, 4, 22, 12, 0),
+    endDate: new Date(2020, 4, 22, 13, 0),
+    allDay: true
+  }, {
+	id: "unique3",
+    title: 'Install New Router in Dev Room',
+    startDate: new Date(2020, 4, 22, 14, 30),
+    endDate: new Date(2020, 4, 22, 15, 30)
+  }
+];
